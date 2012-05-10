d7f6a00d615250bd6b2c43a9852d5ecfee72d176
Luiz Sanches <luizgrsanches@gmail.com>
2012-05-10 10:40:54 -0300

    criacao da classe de acesso aos usuarios

---
+++ dao/UsuarioDAO.class.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname(__FILE__) . '/../general/autoload.php';
+
+class UsuarioDAO extends AbstractDAO {
+  public $id;
+  public $nome;
+  public $email;
+  public $perfis;
+
+  function __construct() {
+    parent::__construct($this);
+  }
+}
+?> 
