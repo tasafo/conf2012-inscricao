933b33f8da43d617e154234824cb697da09e618e
Luiz Sanches <luizgrsanches@gmail.com>
2012-05-11 11:52:27 -0300

    insercao de um usuario admin padrao

--- sqls/banco_inserts.sql
+++ sqls/banco_inserts.sql
@@ -1,2 +1,4 @@
 insert into tipo_inscricao (descricao, valor, status) values ('Valor unico', 55, 'A');
 insert into tipo_inscricao (descricao, valor, status) values ('Cortesia', 0, 'I');
+
+insert into usuario (nome, email, senha, perfis) values ('admin', 'admin@mail.com', MD5('admin'), 'admin');
